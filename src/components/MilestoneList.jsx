9c0f9185a066022acc0ffa41027089f3c0d20395
Rusmir Omerovic <rusmir.omerovic@web.de>
2025-08-02 16:23:31 +0200

    Debug: wrong code in MilestoneList.jsx

--- src/components/MilestoneList.jsx
+++ src/components/MilestoneList.jsx
@@ -25,10 +25,10 @@ const MilestoneList = ({ projectId }) => {
   return (
     <ul className="mt-2 pl-4 list-disc text-sm text-gray-700">
       {milestones.map((m) => (
-      <>
-        <li key={m.id}>{m.title}</li>
-        <li key={m.id}>{m.completed ? "✅ " : "🔘 "}{m.title}</li>
-      </>
+        <li key={m.id}>
+          {m.completed ? "✅ " : "🔘 "}
+          {m.title}
+        </li>
       ))}
     </ul>
   );
